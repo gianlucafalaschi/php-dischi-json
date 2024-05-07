130c8c474fc2e0c45c355491b1c8c73911cd9362
gianlucafalaschi <gian100gian@outlook.it>
2024-05-07 10:49:20 +0200

    comments

--- server.php
+++ server.php
@@ -1,6 +1,6 @@
 <?php
 /* il contenuto (dati in formato json) di discs-list.json viene letto tramite la funzione file_get_contents() 
-che restituisce una stringa  */
+che restituisce una stringa con struttura json */
 $discs_json = file_get_contents('discs-list.json');
 /* il contenuto della stringa viene trasformato in un array associativo di PHP
 tramite la funzione json_decode()  */
@@ -13,7 +13,7 @@ header('Content-Type: application/json');
 
 /* prende struttura dati php e trasforma in json.
  Il contenuto di discs-list.json verra' mandato indietro al client come json 
- (necessario per )*/
+ (necessario per farlo leggere a JS )*/
 
 echo json_encode($discs);
 
